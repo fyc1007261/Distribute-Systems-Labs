b8c2a88872a0cd8f71306875a3bdb63559cfb46d
fyc1007261 <fyc1007261@live.com>
2019-03-08 04:45:51 -0800

    finish window=1

---
+++ rdt_receiver.cc
@@ -0,0 +1,192 @@
+/*
+ * FILE: rdt_receiver.cc
+ * DESCRIPTION: Reliable data transfer receiver.
+ * NOTE: This implementation assumes there is no packet loss, corruption, or 
+ *       reordering.  You will need to enhance it to deal with all these 
+ *       situations.  In this implementation, the packet format is laid out as 
+ *       the following:
+ *       
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
+ *
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <string>
+
+#include "rdt_struct.h"
+#include "rdt_receiver.h"
+
+
+#define LEN_CHECKSUM 2
+#define LEN_MSGSEQ 4
+#define LEN_PKTSEQ 4
+#define LEN_PLDSIZE 1
+#define WINDOW_SIZE 10
+#define TIMEOUT 0.3
+#define HEADER_SIZE (LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ + LEN_PLDSIZE)
+#define PAYLOAD_SIZE (RDT_PKTSIZE - HEADER_SIZE)
+#define MAX(a, b) (a>b?a:b)
+#define MIN(a, b) (a<b?a:b)
+
+struct message_info_buffer {
+    int tot_size;
+    int received_size;
+    std::map<int, std::string> received_pkts;
+    bool sent;
+    message_info_buffer(): tot_size(-1), received_size(0), sent(false){
+        received_pkts.clear();
+    }
+};
+
+static std::map<unsigned int, struct message_info_buffer> messages_buffer;
+static std::map<unsigned int, int> rotations;
+static std::set<struct packet*> forfree;
+
+static int tot_message = 0;
+
+static bool check_checksum(struct packet *pkt){
+    short sum = 0;
+    short got;
+    memcpy(&got, pkt->data, sizeof(short));
+    short *temp = (short*)malloc(sizeof(short));
+    for (int i=2; i<RDT_PKTSIZE; i+=2){
+        memcpy(temp, pkt->data + i, sizeof(short));
+        sum += *temp;
+    }
+    free(temp);
+    return sum == got;
+}
+
+static void checksum(struct packet *pkt){
+    short sum = 0;
+    short *temp = (short*)malloc(sizeof(short));
+    for (int i=2; i<RDT_PKTSIZE; i+=2){
+        memcpy(temp, pkt->data + i, sizeof(short));
+        sum += *temp;
+    }
+    memcpy(pkt->data, &sum, LEN_CHECKSUM);
+    free(temp);
+}
+
+void Receiver_Init()
+{
+    fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
+    messages_buffer.clear();
+    for (unsigned int i=0; i<=255; i++){
+        rotations[i] = 0;
+    }
+    
+}
+
+void Receiver_Final()
+{
+    fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
+    fprintf(stdout, "Total received messages:%d\n", tot_message);
+    for (std::set<struct packet*>::iterator it = forfree.begin(); it!=forfree.end(); it++){
+        free(*it);
+    }
+}
+
+static void send_ack(unsigned int msg_seq, int pkt_seq){
+    struct packet *pkt = (struct packet *)malloc(sizeof(struct packet));
+    memset(pkt->data, 0, RDT_PKTSIZE);
+    memcpy(pkt->data+LEN_CHECKSUM, &msg_seq, LEN_MSGSEQ);
+    memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ, &pkt_seq, LEN_PKTSEQ);
+    checksum(pkt);
+    Receiver_ToLowerLayer(pkt);
+    forfree.insert(pkt);
+}
+
+
+
+static bool message_to_higher(unsigned int msg_seq){
+    // finished, then to the next one;
+    if (messages_buffer.find(msg_seq) == messages_buffer.end())
+        return false;
+    if (messages_buffer[msg_seq].sent) return false;
+    for (unsigned int i=0; i<=255; i++){
+        if (i<msg_seq && rotations[i] <= rotations[msg_seq] ){
+            return false;
+        }
+    }
+    std::string mess("");
+    if (messages_buffer[msg_seq].tot_size != messages_buffer[msg_seq].received_size){
+        return false;
+    }
+    for (unsigned int i=0; i<messages_buffer[msg_seq].received_pkts.size(); ++i){
+        mess += messages_buffer[msg_seq].received_pkts[i];
+    }
+    struct message *msg = (struct message *)malloc(sizeof(struct message));
+    msg->size = messages_buffer[msg_seq].tot_size;
+    msg->data = (char*)malloc(mess.size());
+    strncpy(msg->data, mess.c_str(),mess.size());
+    Receiver_ToUpperLayer(msg);
+    // printf("received:%s\n", msg->data);
+    // messages_buffer.erase(msg_seq);
+    messages_buffer[msg_seq].sent = true;
+    rotations[msg_seq] += 1;
+    tot_message ++;
+    return true;
+}
+
+/* event handler, called when a packet is passed from the lower layer at the 
+   receiver */
+void Receiver_FromLowerLayer(struct packet *pkt)
+{
+    // printf("received a packet\n");
+    if (!check_checksum(pkt)){
+        // printf("checksum fails.\n");
+        return;
+    }
+    unsigned int msg_seq;
+    unsigned char payload_size;
+    int pkt_seq;
+    char payload[PAYLOAD_SIZE];
+    memset(payload, 0, PAYLOAD_SIZE);
+
+    memcpy(&msg_seq, pkt->data+LEN_CHECKSUM, LEN_MSGSEQ);
+    memcpy(&pkt_seq, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ, LEN_PKTSEQ);
+    memcpy(&payload_size, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ+LEN_PKTSEQ, LEN_PLDSIZE);
+
+    send_ack(msg_seq, pkt_seq);
+
+    if (messages_buffer.find(msg_seq) != messages_buffer.end() && messages_buffer[msg_seq].received_pkts.find(pkt_seq) != messages_buffer[msg_seq].received_pkts.end())
+        return;
+
+    if (messages_buffer.find(msg_seq) == messages_buffer.end()){
+        // add new message 
+        messages_buffer.insert(std::make_pair(msg_seq, message_info_buffer()));
+    }
+
+    if (pkt_seq == 0){
+        // different packet structure
+        int msg_size;
+        memcpy(&msg_size, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ+LEN_PKTSEQ+LEN_PLDSIZE, 4);
+        memcpy(payload, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ+LEN_PKTSEQ+LEN_PLDSIZE+4, payload_size);
+        std::string payload_str(payload);
+        messages_buffer[msg_seq].tot_size = msg_size;
+        messages_buffer[msg_seq].received_size += payload_size;
+        messages_buffer[msg_seq].received_pkts.insert(std::make_pair(pkt_seq,payload_str));
+    }
+    else{
+        memcpy(payload, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ+LEN_PKTSEQ+LEN_PLDSIZE, payload_size);
+        std::string payload_str(payload);
+        messages_buffer[msg_seq].received_size += payload_size;
+        messages_buffer[msg_seq].received_pkts.insert(std::make_pair(pkt_seq,payload_str));
+    }
+
+  
+    if (message_to_higher(msg_seq)){
+        for (unsigned int i = 0; i<=255; i++){
+            if (!message_to_higher(i)) ;
+        }
+    }
+  
+}
---
+++ rdt_sender.cc
@@ -0,0 +1,221 @@
+/*
+ * FILE: rdt_sender.cc
+ * DESCRIPTION: Reliable data transfer sender.
+ * NOTE: This implementation assumes there is no packet loss, corruption, or 
+ *       reordering.  You will need to enhance it to deal with all these 
+ *       situations.  In this implementation, the packet format is laid out as 
+ *       the following:
+ *       
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
+ *
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
+ */
+/*
+ *  new design:
+ *  checksum(1 byte) + message_seq#(1 byte) + packet_seq#(4 bytes) + payload_size(1 byte) + contents(121 *  bytes)
+ * 
+ * 
+*/
+#define LEN_CHECKSUM 2
+#define LEN_MSGSEQ 4
+#define LEN_PKTSEQ 4
+#define LEN_PLDSIZE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <pthread.h>
+#include <unistd.h>
+
+
+#include "rdt_struct.h"
+#include "rdt_sender.h"
+
+#define WINDOW_SIZE 1
+#define TIMEOUT 0.3
+#define HEADER_SIZE (LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ + LEN_PLDSIZE)
+#define PAYLOAD_SIZE (RDT_PKTSIZE - HEADER_SIZE)
+
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a < b ? a : b)
+
+static std::map<long, struct pkt_info> send_buffer_map;
+static int tot_msg = 0;
+static int tot_pkt = 0;
+
+
+static int nack;
+// static pthread_mutex_t mutex;
+
+struct pkt_info
+{
+    struct packet *pkt;
+    bool acked;
+    pkt_info() : pkt(NULL), acked(false) {}
+};
+
+static bool check_checksum(struct packet *pkt){
+    short sum = 0;
+    short got;
+    memcpy(&got, (char*)pkt->data, sizeof(short));
+    short *temp = (short*)malloc(sizeof(short));
+    for (int i=2; i<RDT_PKTSIZE; i+=2){
+        memcpy(temp, (char*)pkt->data + i, sizeof(short));
+        sum += *temp;
+    }
+    free(temp);
+    return sum == got;
+}
+static void checksum(struct packet *pkt)
+{
+    short sum = 0;
+    short *temp = (short *)malloc(sizeof(short));
+    for (int i = 2; i < RDT_PKTSIZE; i += 2)
+    {
+        memcpy(temp, (char *)pkt->data + i, sizeof(short));
+        sum += *temp;
+    }
+    memcpy(pkt->data, &sum, LEN_CHECKSUM);
+    free(temp);
+}
+
+static int waiting;
+void flush_outof_queue(){
+    if (waiting!=0 || send_buffer_map.size()<WINDOW_SIZE) return;
+    int i = 0;
+    waiting = WINDOW_SIZE;
+
+    for (auto it = send_buffer_map.begin(); i<WINDOW_SIZE; i++, it++){
+        // printf("size=%d\n", send_buffer_map.size());
+        tot_pkt++;
+        Sender_ToLowerLayer(it->second.pkt);
+    }
+    Sender_StartTimer(TIMEOUT);
+}
+
+void Sender_Init()
+{
+    // pthread_mutex_init(&mutex, NULL);
+    waiting = 0;
+    nack = 0;
+    fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
+}
+
+void Sender_Final()
+{
+    fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
+    printf("Total pkts:%d\n", tot_pkt);
+    printf("Total acks:%d\n", nack);
+    printf("Total messages:%d\n", tot_msg);
+
+    for (std::map<long, struct pkt_info>::iterator it = send_buffer_map.begin(); it != send_buffer_map.end(); it++)
+    {
+        free(it->second.pkt);
+    }
+}
+
+/* event handler, called when a message is passed from the upper layer at the 
+   sender */
+static unsigned int msg_seq = 0;
+
+void Sender_FromUpperLayer(struct message *msg)
+{
+    // 4 bytes for the message size in the first packet.
+    int pkt_needed = ((msg->size + 4) % PAYLOAD_SIZE == 0) ? (int)((msg->size + 4) / PAYLOAD_SIZE) : (int)((msg->size + 4) / PAYLOAD_SIZE) + 1;
+    tot_msg++;
+
+    int rest_size = msg->size;
+    int cursor = 0;
+
+    // printf("message:%s\n", msg->data);
+
+    for (int i = 0; i < pkt_needed; ++i)
+    {
+        struct packet *pkt = (struct packet *)malloc(sizeof(struct packet));
+        memset(pkt->data, 0, RDT_PKTSIZE);
+        int pkt_seq = i;
+        if (i == 0)
+        {
+            unsigned char pldsize = rest_size > (PAYLOAD_SIZE - 4) ? (PAYLOAD_SIZE - 4) : rest_size;
+            memcpy(pkt->data + LEN_CHECKSUM, &msg_seq, LEN_MSGSEQ);
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ, &pkt_seq, LEN_PKTSEQ);
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ, &pldsize, LEN_PLDSIZE);
+            // additional message size
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ + LEN_PLDSIZE, &(msg->size), sizeof(int));
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ + LEN_PLDSIZE + sizeof(int), msg->data + cursor, pldsize);
+
+            checksum(pkt);
+
+            rest_size -= pldsize;
+            cursor += pldsize;
+
+            long index = (msg_seq << 16) + pkt_seq;
+            struct pkt_info info = pkt_info();
+            info.pkt = pkt;
+            send_buffer_map[index] = info;
+        }
+        else
+        {
+            // char pkt_size = MIN(rest_size + HEADER_SIZE, RDT_PKTSIZE);
+            unsigned char pldsize = MIN(PAYLOAD_SIZE, rest_size);
+            memcpy(pkt->data + LEN_CHECKSUM, &msg_seq, LEN_MSGSEQ);
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ, &pkt_seq, LEN_PKTSEQ);
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ, &pldsize, LEN_PLDSIZE);
+            memcpy(pkt->data + LEN_CHECKSUM + LEN_MSGSEQ + LEN_PKTSEQ + LEN_PLDSIZE, msg->data + cursor, pldsize);
+
+            checksum(pkt);
+
+            rest_size -= pldsize;
+            cursor += pldsize;
+
+            long index = (msg_seq << 16) + pkt_seq;
+            struct pkt_info info = pkt_info();
+            info.pkt = pkt;
+            send_buffer_map[index] = info;
+        }
+        // printf("sendpkt:%s\n", pkt->data+12);
+        // buff_queue.push(pkt);
+    }
+    flush_outof_queue();
+    
+    ++msg_seq;
+
+    
+}
+
+/* event handler, called when a packet is passed from the lower layer at the 
+   sender */
+void Sender_FromLowerLayer(struct packet *pkt)
+{
+    // printf("sender from lower!\n");
+    nack++;
+    if (!check_checksum(pkt))
+    {
+        return;
+    }
+    unsigned int msg_seq;
+    int pkt_seq;
+    memcpy(&msg_seq, pkt->data+LEN_CHECKSUM, LEN_MSGSEQ);
+    memcpy(&pkt_seq, pkt->data+LEN_CHECKSUM+LEN_MSGSEQ, LEN_PKTSEQ);
+    long index = (msg_seq << 16) + pkt_seq;
+    // send_buffer_map[index].acked = true;
+    send_buffer_map.erase(index);
+    waiting--;
+    if (waiting==0) {
+        Sender_StopTimer();
+        flush_outof_queue();
+    }
+}
+
+/* event handler, called when the timer expires */
+void Sender_Timeout()
+{
+    waiting = 0;
+    flush_outof_queue();
+}
